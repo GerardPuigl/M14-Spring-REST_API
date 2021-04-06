3a2d0c3fbbc55188d6e24fb8ae793e1af4c0ca11
Gerard <gerardpuig31@hotmail.com>
2021-04-06 19:31:14 +0200

    Functionalities Scheme

--- src/main/java/com/itacademy/CrudPictures/controller/ControllerPictures.java
+++ src/main/java/com/itacademy/CrudPictures/controller/ControllerPictures.java
@@ -1,16 +1,37 @@
 package com.itacademy.CrudPictures.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.itacademy.CrudPictures.domain.Shop;
+import com.itacademy.CrudPictures.domain.Picture;
+
 
 @RestController
+@RequestMapping("/shop/{ID}")
 public class ControllerPictures {
 	
-	@PostMapping("/shops")
-	public Shop shopCreation() {
+	@PostMapping("/pictures")
+	public Picture addPicture(@Valid @RequestBody Picture employee, @PathVariable("id") int id) {
 		return null;
 	}
-
+	
+	@GetMapping("/pictures")
+	public List<Picture> getAllPictures(@PathVariable("id") int id){
+		return null;
+	}
+	
+	@DeleteMapping("/pictures")
+	public String deleteAll(@PathVariable("id") int id) {
+		return "Tots els quadres han estat eliminats";
+		}
+	
 }
--- src/main/java/com/itacademy/CrudPictures/controller/ControllerShops.java
+++ src/main/java/com/itacademy/CrudPictures/controller/ControllerShops.java
@@ -1,10 +1,23 @@
 package com.itacademy.CrudPictures.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itacademy.CrudPictures.domain.Shop;
+
 @RestController
-@RequestMapping("shop")
 public class ControllerShops {
 
+	@PostMapping("/shops")
+	public Shop shopCreation() {
+		return null;
+	}
+	
+	@GetMapping("/shops")
+	public List<Shop> allShops(){
+		return null;
+	}
 }
